9b8c10ec20450bc605df566dd83422763ebff194
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2024-12-23 00:58:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import util.HttpRequest;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
